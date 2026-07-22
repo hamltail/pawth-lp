ce3864bd1820137e3a8fc97c731eae35fcab50db
h-waji <goo.hiro.w@gmail.com>
2026-07-22 11:15:57 +0900

    Conceptセクションを実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import Screens from "../components/Screens";
+import Concept from "../components/Concept";
 
 export default function Home() {
   return (
     <main className="page">
       <Hero />
       <Screens />
+      <Concept />
     </main>
   );
 }
